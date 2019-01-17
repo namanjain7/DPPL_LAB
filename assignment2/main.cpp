776a4f86e1e31dd85c0a9e7cda86f7d440563e0c
Shreyash H. Turkar <33600638+Shreyash7478@users.noreply.github.com>
2019-01-17 17:51:47 +0530

    Create main.cpp

---
+++ assignment2/main.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include"bst.h"
+
+int main(){
+	bst bst1;
+	bst1.insert(10);
+	bst1.insert(5);
+	bst1.insert(11);
+	bst1.insert(6);
+	bst1.insert(1);
+	bst1.insert(20);
+	bst1.insert(-1);
+	bst1.inorder();
+	
+	bst bst2;
+	bst2.insert(10);
+	bst2.insert(5);
+	bst2.insert(11);
+	bst2.insert(12);
+	bst2.insert(1);
+	bst2.insert(20);
+	bst2.insert(-1);
+	bst2.inorder();
+	
+	std::cout<<(bst1.is_identical(bst2))<<std::endl;
+	
+	
+
+}
